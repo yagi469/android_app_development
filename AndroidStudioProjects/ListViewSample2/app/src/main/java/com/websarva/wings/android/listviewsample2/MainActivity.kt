27266066ad72568dd618279e8fb93b3097199f64
yaginumatti <ryohei122yagi@gmail.com>
2022-09-19 16:36:11 +0900

    create ListViewSample2

---
+++ AndroidStudioProjects/ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.websarva.wings.android.listviewsample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // ListViewオブジェクトを取得。
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        // リストビューに表示するリストデータを作成。
+        var menuList = mutableListOf("から揚げ定食", "ハンバーグ定食", "生姜焼き定食",
+        "ステーキ定食", "野菜炒め定食", "とんかつ定食", "ミンチかつ定食", "チキンカツ定食",
+        "コロッケ定食", "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "焼き魚定食", "焼肉定食")
+        // アダプタオブジェクトを生成。
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1,
+        menuList)
+        // リストビューにアダプタオブジェクトを設定。
+        lvMenu.adapter = adapter
+        // リストビューにリスナを設定。
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    // リストがタップされたときの処理が記述されたメンバクラス。
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            // 注文確認ダイアログフラグメントオブジェクトを生成。
+            val dialogFragment = OrderConfirmDialogFragment()
+            // ダイアログ表示。
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDIalogFragment")
+        }
+    }
+}
---
+++ AndroidStudioProjects/ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/OrderConfirmDialogFragment.kt
@@ -0,0 +1,55 @@
+package com.websarva.wings.android.listviewsample2
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+
+class OrderConfirmDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // アクティビティがnullでないならばダイアログオブジェクトを生成。
+        val dialog = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            // ダイアログのタイトルを設定。
+            builder.setTitle(R.string.dialog_title)
+            // ダイアログのメッセージを設定。
+            builder.setMessage(R.string.dialog_msg)
+            // Positive Buttonを設定。
+            builder.setPositiveButton(R.string.dialog_btn_ok, DialogButtonClickListener())
+            // Negative Buttonを設定。
+            builder.setNegativeButton(R.string.dialog_btn_ng, DialogButtonClickListener())
+            // Neutral Buttonを設定。
+            builder.setNeutralButton(R.string.dialog_btn_nu, DialogButtonClickListener())
+            // ダイアログオブジェクトを生成。
+            builder.create()
+        }
+        // 生成したダイアログオブジェクトをリターン。
+        return dialog ?: throw IllegalStateException("アクティビティがnullです")
+    }
+    // ダイアログのアクションボタンがタップされた時の処理が記述されたメンバクラス。
+    private inner class DialogButtonClickListener : DialogInterface.OnClickListener {
+        override fun onClick(dialog: DialogInterface, which: Int) {
+            // トーストメッセージ用文字列変数を用意。
+            var msg = ""
+            // タップされたアクションボタンで分岐。
+            when(which) {
+            // Positive Buttonならば…
+            DialogInterface.BUTTON_POSITIVE ->
+                // 注文用のメッセージを格納。
+                msg = getString(R.string.dialog_ok_toast)
+            // Negative Buttonならば…
+            DialogInterface.BUTTON_NEGATIVE ->
+                // キャンセル用のメッセージを格納。
+                msg = getString(R.string.dialog_ng_toast)
+            // Neutral Buttonならば…
+            DialogInterface.BUTTON_NEUTRAL ->
+                // 問合せ用のメッセージを格納。
+                msg = getString(R.string.dialog_nu_toast)
+            }
+            // トーストの表示。
+            Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
+        }
+    }
+}
