f5a8961fe4ffa8401106461ff9c7dd19f2d4bc22
yaginumatti <ryohei122yagi@gmail.com>
2022-09-22 16:51:43 +0900

    コルーチンに関するコードを記述する

--- AndroidStudioProjects/AsyncCoroutineSample/app/src/main/java/com/websarva/wings/android/asynccoroutinesample/MainActivity.kt
+++ AndroidStudioProjects/AsyncCoroutineSample/app/src/main/java/com/websarva/wings/android/asynccoroutinesample/MainActivity.kt
@@ -10,6 +10,10 @@ import android.widget.SimpleAdapter
 import android.widget.TextView
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
@@ -85,29 +89,36 @@ class MainActivity : AppCompatActivity() {
     @UiThread
     private fun receiveWeatherInfo(urlFull: String) {
         // ここにコルーチンに関するコードを記述
+        lifecycleScope.launch {
+            val result = weatherInfoBackgroundRunner(urlFull)
+            weatherInfoPostRunner(result)
+        }
     }
 
     @WorkerThread
-    private fun weatherInfoBackgroundRunner(url: String): String {
-        var result = ""
-        val url = URL(url)
-        val con = url.openConnection() as? HttpURLConnection
-        con?.let {
-            try {
-                it.connectTimeout = 1000
-                it.readTimeout = 1000
-                it.requestMethod = "GET"
-                it.connect()
-                val stream = it.inputStream
-                result = is2String(stream)
-                stream.close()
+    private suspend fun weatherInfoBackgroundRunner(url: String): String {
+        val returnVal = withContext(Dispatchers.IO) {
+            var result = ""
+            val url = URL(url)
+            val con = url.openConnection() as? HttpURLConnection
+            con?.let {
+                try {
+                    it.connectTimeout = 1000
+                    it.readTimeout = 1000
+                    it.requestMethod = "GET"
+                    it.connect()
+                    val stream = it.inputStream
+                    result = is2String(stream)
+                    stream.close()
+                }
+                catch(ex: SocketTimeoutException) {
+                    Log.w(DEBUG_TAG, "通信タイムアウト", ex)
+                }
+                it.disconnect()
             }
-            catch(ex: SocketTimeoutException) {
-                Log.w(DEBUG_TAG, "通信タイムアウト", ex)
-            }
-            it.disconnect()
+            result
         }
-        return result
+        return returnVal
     }
 
     private fun is2String(stream: InputStream): String {
