ae354a0ba9f75f3569bb76409a1a755a88289f4d
yaginumatti <ryohei122yagi@gmail.com>
2022-09-23 17:34:53 +0900

    位置情報機能利用の許可とパーミッションチェックのコードを記述

--- AndroidStudioProjects/ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
+++ AndroidStudioProjects/ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.websarva.wings.android.implicitintentsample
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +18,31 @@ class MainActivity : AppCompatActivity() {
     private var _latitude = 0.0
     // 経度プロパティ。
     private var _longitude = 0.0
+    // FusedLocationProviderClientオブジェクトプロパティ。
+    private lateinit var _fusedLocationClient: FusedLocationProviderClient
+    // LocationRequestオブジェクトプロパティ。
+    private lateinit var _locationRequest: LocationRequest
+    // 位置情報が変更されたときの処理を行うコールバックオブジェクトプロパティ。
+    private lateinit var _onUpdateLocation: OnUpdateLocation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // FusedLocationProviderClientオブジェクトを取得。
+        _fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        // LocationRequestオブジェクトを生成。
+        _locationRequest = LocationRequest.create()
+        // LocationRequestオブジェクトがnullでなければ…
+        _locationRequest?.let {
+            // 位置情報の更新間隔を設定。
+            it.interval = 5000
+            // 位置情報の最短更新間隔を設定。
+            it.fastestInterval = 1000
+            // 位置情報の取得精度を設定。
+            it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        // 位置情報が変更されたときの処理を行うコールバックオブジェクトを生成。
+        _onUpdateLocation = OnUpdateLocation()
     }
 
     fun onMapSearchButtonClick(view: View) {
@@ -44,5 +70,65 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_VIEW, uri)
         // アクティビティを起動。
         startActivity(intent)
+    }
+
+    private inner class OnUpdateLocation : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult?.let {
+                // 直近の位置情報を取得。
+                val location = it.lastLocation
+                location?.let {
+                    // locationオブジェクトから緯度を取得。
+                    _latitude = it.latitude
+                    // locationオブジェクトから経度を取得。
+                    _longitude = it.longitude
+                    // 取得した緯度をTextViewに表示。
+                    val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
+                    tvLatitude.text = _latitude.toString()
+                    // 取得した経度をTextViewに表示。
+                    val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
+                    tvLongitude.text = _longitude.toString()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // ACCESS_FINE_LOCATIONの許可が下りていないなら…
+        if(ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.
+    ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // ACCESS_FINE_LOCATIONの許可を求めるダイアログを表示。その際、リクエストコードを1000に設定。
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this@MainActivity, permissions, 1000)
+            // onResume()メソッドを終了。
+            return
+        }
+        // 位置情報の追跡を開始。
+        _fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation,
+    mainLooper)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // 位置情報の追跡を停止。
+        _fusedLocationClient.removeLocationUpdates(_onUpdateLocation)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+    grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // ACCESS_FINE_LOCATIONに対するパーミッションダイアログでかつ許可を選択したなら…
+        if(requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // 再度ACCESS_FINE_LOCATIONの許可が下りていないかどうかのチェックをし、下りていないなら処理を中止。
+            if(ActivityCompat.checkSelfPermission(this@MainActivity,
+        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            // 位置情報の追跡を開始。
+            _fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation,
+        mainLooper)
+        }
     }
 }
