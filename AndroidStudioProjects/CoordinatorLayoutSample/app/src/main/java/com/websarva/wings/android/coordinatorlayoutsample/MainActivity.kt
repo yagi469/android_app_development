0be8d494fa20d6ab9262add1b9e432a4769e405b
yaginumatti <ryohei122yagi@gmail.com>
2022-09-24 17:07:50 +0900

    CollapsingToolbarLayoutにタイトルを設定。

--- AndroidStudioProjects/CoordinatorLayoutSample/app/src/main/java/com/websarva/wings/android/coordinatorlayoutsample/MainActivity.kt
+++ AndroidStudioProjects/CoordinatorLayoutSample/app/src/main/java/com/websarva/wings/android/coordinatorlayoutsample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +15,15 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         // ツールバーにロゴを設定。
         toolbar.setLogo(R.mipmap.ic_launcher)
-        // ツールバーにタイトル文字列を設定。
-        toolbar.setTitle(R.string.toolbar_title)
-        // ツールバーのタイトル文字色を設定。
-        toolbar.setTitleTextColor(Color.WHITE)
-        // ツールバーのサブタイトル文字列を設定。
-        toolbar.setSubtitle(R.string.toolbar_subtitle)
-        // ツールバーのサブタイトル文字色を設定。
-        toolbar.setSubtitleTextColor(Color.LTGRAY)
         // アクションバーにツールバーを設定。
         setSupportActionBar(toolbar)
+        // CollapsingToolbarLayoutを取得。
+        val toolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.toolbarLayout)
+        // タイトルを設定。
+        toolbarLayout.title = getString(R.string.toolbar_title)
+        // 通常サイズ時の文字色を設定。
+        toolbarLayout.setExpandedTitleColor(Color.WHITE)
+        // 縮小サイズ時の文字色を設定。
+        toolbarLayout.setCollapsedTitleTextColor(Color.LTGRAY)
     }
 }
