5538f39463431c53e0e59b0c6ec85113b54f98b6
yaginumatti <ryohei122yagi@gmail.com>
2022-09-24 16:37:32 +0900

    ツールバーの導入

--- AndroidStudioProjects/ToolbarSample/app/src/main/java/com/websarva/wings/android/toolbarsample/MainActivity.kt
+++ AndroidStudioProjects/ToolbarSample/app/src/main/java/com/websarva/wings/android/toolbarsample/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.websarva.wings.android.toolbarsample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Toolbarを取得。
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // ツールバーにロゴを設定。
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        // ツールバーにタイトル文字列を設定。
+        toolbar.setTitle(R.string.toolbar_title)
+        // ツールバーのタイトル文字色を設定。
+        toolbar.setTitleTextColor(Color.WHITE)
+        // ツールバーのサブタイトル文字列を設定。
+        toolbar.setSubtitle(R.string.toolbar_subtitle)
+        // ツールバーのサブタイトル文字色を設定。
+        toolbar.setSubtitleTextColor(Color.LTGRAY)
+        // アクションバーにツールバーを設定。
+        setSupportActionBar(toolbar)
     }
 }
